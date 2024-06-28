73754d5caead8e8131c3d3912537fc8471f576eb
Saurabh Pandey <saurabh544321@gmail.com>
2024-06-28 16:30:47 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+class doctor(models.Model):
+    FName=models.CharField(max_length=200,null=False,blank=False)
+    LName=models.CharField(max_length=200,null=False,blank=False)
+    Password=models.CharField(max_length=20,null=False,blank=False)
+    Email=models.EmailField(null=False,blank=False)
+    Address=models.CharField(max_length=100,null=False,blank=False)
+    Profile=models.ImageField(upload_to="images")
+    Username=models.CharField(max_length=100,null=False,blank=False)
+   
+
+    def __str__(self) -> str:
+        return self.Username
+
+class Patient(models.Model):
+    FName=models.CharField(max_length=200,null=False,blank=False)
+    LName=models.CharField(max_length=200,null=False,blank=False)
+    Password=models.CharField(max_length=20,null=False,blank=False)
+    Email=models.EmailField(null=False,blank=False)
+    Address=models.CharField(max_length=100,null=False,blank=False)
+    Profile=models.ImageField(upload_to="images")
+    Username=models.CharField(max_length=100,null=False,blank=False)
+   
+
+    def __str__(self) -> str:
+        return self.Username
